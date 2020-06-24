5aad20441c86d3d30e7285f9978d900d42e26acb
AR123456 <coloradoz99@gmail.com>
2020-06-23 20:40:02 -0600

    setting up to deploy

---
+++ build/precache-manifest.0084e3ad0905528ccac91d0931eb1b49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c34f5d5f11896071618481c2067ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "132b4118f3b84f07bc98",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d6b49729e7ae2ba4e67b",
+    "url": "/static/css/main.29b70919.chunk.css"
+  },
+  {
+    "revision": "132b4118f3b84f07bc98",
+    "url": "/static/js/2.ea328980.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.ea328980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b49729e7ae2ba4e67b",
+    "url": "/static/js/main.50385f97.chunk.js"
+  },
+  {
+    "revision": "16212dc685354c0d4ed6",
+    "url": "/static/js/runtime-main.28654420.js"
+  },
+  {
+    "revision": "f52599e7ab91882a3fc647cdcc948090",
+    "url": "/static/media/nurseandlady.f52599e7.jpeg"
+  }
+]);
